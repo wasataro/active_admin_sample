ecb2ea8876f5a9f8deb1e2a36fa7c406b8c30aa6
阿部宏祐 <abekosuke@kosukeMBP.local>
2020-03-19 02:48:28 +0900

    rails課題５を追加

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module ActiveAdminSample
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+  end
+end
